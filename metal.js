412ac2ee74d87edf1e09594b5a96d5b8501ed12d
eredacokmerke <eredacokmerke@gmail.com>
2015-01-01 17:42:23 +0200

    26H2

--- metal.js
+++ metal.js
@@ -1,4 +1,5 @@
-function metal_init(options) {
+function metal_init(options)
+{
     var INDEKS = "indeks";
     var ICERIK_DIV = "metal-icerik-div"; //input/output
     var INPUT_DIV = "metal-input-div";
@@ -13,13 +14,17 @@ function metal_init(options) {
     var padding = "10px";
     var metal_div = document.getElementsByClassName("metal-div");
     var ONE_LIST_VISIBLE;
-    if (options === undefined) {
+    if (options === undefined) //init metodunda belirtilmemiş, öntanımlı değer kullanılacak
+    {
         ONE_LIST_VISIBLE = false;
-    } else {
+    }
+    else
+    {
         ONE_LIST_VISIBLE = options.oneListVisible;
     }
 
-    function metalDiv(type, isaret, liste, secenek, icerik, ilkInput) {
+    function metalDiv(type, isaret, liste, secenek, icerik, ilkInput)
+    {
         this.type = type;
         this.isaret = isaret;
         this.liste = liste;
@@ -30,7 +35,8 @@ function metal_init(options) {
 
     var metalDivList = [];
 
-    for (var indeks = 0; indeks < metal_div.length; indeks++) {
+    for (var indeks = 0; indeks < metal_div.length; indeks++)
+    {
         var secenek = metal_div[indeks].children[0]; //metal-div in içindeki 1. kısım (input/output+pointer)
         var liste = metal_div[indeks].children[1]; //metal-div in içindeki 2. kısım (liste)
         liste.setAttribute(INDEKS, indeks);
@@ -41,7 +47,8 @@ function metal_init(options) {
         var aa = toplamEn - 25;
         var ab = aa + "px";
 
-        if (secenek.className === INPUT_DIV) {
+        if (secenek.className === INPUT_DIV)
+        {
             var icerik = document.createElement("input");
             icerik.setAttribute("value", secenek.getAttribute(TEXT));
             icerik.className = INPUT;
@@ -77,7 +84,9 @@ function metal_init(options) {
 
             var metaldiv = new metalDiv(INPUT_DIV, div2, liste, secenek, icerik);
             metalDivList.push(metaldiv);
-        } else if (secenek.className === OUTPUT_DIV) {
+        }
+        else if (secenek.className === OUTPUT_DIV)
+        {
             var icerik = document.createElement("p");
             icerik.innerHTML = secenek.getAttribute(TEXT);
             icerik.className = INPUT;
@@ -119,71 +128,90 @@ function metal_init(options) {
         }
     }
 
-    for (var i = 0; i < metalDivList.length; i++) {
+    for (var i = 0; i < metalDivList.length; i++)
+    {
         //icerige tıklandı. 
-        if (metalDivList[i].type === INPUT_DIV) {
+        if (metalDivList[i].type === INPUT_DIV)
+        {
             //listeden seçim yapıldı
-            metalDivList[i].liste.addEventListener("click", function (e) {
+            metalDivList[i].liste.addEventListener("click", function(e)
+            {
                 var ii = this.getAttribute(INDEKS);
-                metalDivList[ii].icerik.value = e.target.innerHTML;
+                metalDivList[ii].icerik.value = yaziyiGetir(e.target);
                 listeyiKapat(ii);
                 metalDivList[ii].ilkInput = 1;
             });
 
             //tip input ise input ilk tıklamada temizlenecek
-            metalDivList[i].icerik.addEventListener("click", function () {
+            metalDivList[i].icerik.addEventListener("click", function()
+            {
                 var ii = this.getAttribute(INDEKS);
-                if (metalDivList[ii].ilkInput === 1) {
+                if (metalDivList[ii].ilkInput === 1)
+                {
                     metalDivList[ii].ilkInput = 0;
                     metalDivList[ii].icerik.value = "";
                 }
             });
 
             //tip input ise isarete tıklayınca liste açılacak
-            metalDivList[i].isaret.addEventListener("click", function () {
+            metalDivList[i].isaret.addEventListener("click", function()
+            {
                 var ii = this.getAttribute(INDEKS);
-                if (metalDivList[ii].liste.style.display === "none") {
+                if (metalDivList[ii].liste.style.display === "none")
+                {
                     listeyiAc(ii);
-                    if (ONE_LIST_VISIBLE === true) {
+                    if (ONE_LIST_VISIBLE === true)
+                    {
                         digerListeleriKapat(ii);
                     }
-                } else {
+                }
+                else
+                {
                     listeyiKapat(ii);
                 }
             });
 
             //input a yazı yazıldı
-            metalDivList[i].icerik.addEventListener("input", function () {
+            metalDivList[i].icerik.addEventListener("input", function()
+            {
                 var ii = this.getAttribute(INDEKS);
                 var sonucVar = 0; //listeyi açıp kapatabilmek için
-
                 metalDivList[ii].ilkInput = 0;
-
                 var liste = metalDivList[ii].liste.getElementsByTagName("li");
 
-                for (var i = 0; i < liste.length; i++) {
+                for (var i = 0; i < liste.length; i++)
+                {
                     var yazi = yaziyiGetir(liste[i]);
 
-                    if (yazi.toLowerCase().indexOf(this.value.toLowerCase()) === -1) {
+                    if (yazi.toLowerCase().indexOf(this.value.toLowerCase()) === -1)
+                    {
                         liste[i].style.opacity = 0;
                         liste[i].style.display = "none";
-                    } else {
+                    }
+                    else
+                    {
                         liste[i].style.opacity = 1;
                         liste[i].style.display = "";
                         sonucVar = 1;
                     }
 
-                    if (sonucVar === 1) {
+                    if (sonucVar === 1)
+                    {
                         listeyiAc(ii);
-                    } else {
+                    }
+                    else
+                    {
                         listeyiKapat(ii);
                     }
                 }
             });
 
-        } else if (metalDivList[i].type === OUTPUT_DIV) {
+        }
+        else if (metalDivList[i].type === OUTPUT_DIV)
+        {
             //listeden seçim yapıldı
-            metalDivList[i].liste.addEventListener("click", function (e) {
+            metalDivList[i].liste.addEventListener("click", function(e)
+            {
                 var ii = this.getAttribute(INDEKS);
                 metalDivList[ii].icerik.innerHTML = e.target.innerHTML;
                 listeyiKapat(ii);
@@ -191,14 +219,19 @@ function metal_init(options) {
             });
 
             //tip output ise isarete ve outputa tıklayınca liste açılacak
-            metalDivList[i].secenek.addEventListener("click", function () {
+            metalDivList[i].secenek.addEventListener("click", function()
+            {
                 var ii = this.getAttribute(INDEKS);
-                if (metalDivList[ii].liste.style.display === "none") {
+                if (metalDivList[ii].liste.style.display === "none")
+                {
                     listeyiAc(ii);
-                    if (ONE_LIST_VISIBLE === true) {
+                    if (ONE_LIST_VISIBLE === true)
+                    {
                         digerListeleriKapat(ii);
                     }
-                } else {
+                }
+                else
+                {
                     listeyiKapat(ii);
                 }
             });
@@ -206,52 +239,66 @@ function metal_init(options) {
     }
 
     //liste haricine tıklanırsa liste kapanacak
-    window.onclick = function (e) {
+    window.onclick = function(e)
+    {
         e = e || window.event;
         var target = e.target || e.srcElement;
         var id = target.parentNode.id;
-        if (target.parentNode.className !== INPUT_DIV && target.parentNode.className !== ITEMS && target.parentNode.className !== OUTPUT_DIV && target.parentNode.className !== POINTER_DIV && target.parentNode.className !== ICERIK_DIV) {
+        if (target.parentNode.className !== INPUT_DIV && target.parentNode.className !== ITEMS && target.parentNode.className !== OUTPUT_DIV && target.parentNode.className !== POINTER_DIV && target.parentNode.className !== ICERIK_DIV)
+        {
             listeleriKapat();
         }
     };
 
     //acik olan listeleri kapatir
-    function listeleriKapat() {
-        for (var i = 0; i < metalDivList.length; i++) {
+    function listeleriKapat()
+    {
+        for (var i = 0; i < metalDivList.length; i++)
+        {
             listeyiKapat(i);
         }
     }
 
-    function digerListeleriKapat(ind) {
-        for (var i = 0; i < metalDivList.length; i++) {
-            if (i != ind) {
+    function digerListeleriKapat(ind)
+    {
+        for (var i = 0; i < metalDivList.length; i++)
+        {
+            if (i != ind)
+            {
                 listeyiKapat(i);
             }
         }
     }
 
     //iç etiketlerden yazıları getirir
-    function yaziyiGetir(eleman) {
+    function yaziyiGetir(eleman)
+    {
         var a = 0;
         var icElemanSayisi = eleman.children.length;
         var yazi = "";
 
-        if (icElemanSayisi === 0) {
+        if (icElemanSayisi === 0)
+        {
             yazi = eleman.innerHTML;
-        } else {
-            for (a = 0; a < icElemanSayisi; a++) {
+        }
+        else
+        {
+            for (a = 0; a < icElemanSayisi; a++)
+            {
                 yazi = yazi + yaziyiGetir(eleman.children[a]);
             }
         }
         return yazi;
     }
 
-    function listeyiKapat(ind) {
+    function listeyiKapat(ind)
+    {
         metalDivList[ind].liste.style.display = "none";
         metalDivList[ind].isaret.children[0].innerHTML = ISARET_AC;
     }
 
-    function listeyiAc(ind) {
+    function listeyiAc(ind)
+    {
         metalDivList[ind].liste.style.display = "inline";
         metalDivList[ind].isaret.children[0].innerHTML = ISARET_KAPA;
     }
